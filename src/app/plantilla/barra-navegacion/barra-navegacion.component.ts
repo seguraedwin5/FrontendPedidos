3b985327945242babe405c90ba65b612638f9455
seguraedwin5 <51142779+seguraedwin5@users.noreply.github.com>
2022-11-16 17:43:11 -0500

    creacion frontend

---
+++ src/app/plantilla/barra-navegacion/barra-navegacion.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+var sidenavbar = require("../../../assets/js/navs.js")
+@Component({
+  selector: 'app-barra-navegacion',
+  templateUrl: './barra-navegacion.component.html',
+  styleUrls: ['./barra-navegacion.component.css']
+})
+export class BarraNavegacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  
+      
+  }
+
+}
