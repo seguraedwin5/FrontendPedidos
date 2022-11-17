0ff3be3d04dfe72366fc1762b99cabed18caa38e
seguraedwin5 <51142779+seguraedwin5@users.noreply.github.com>
2022-11-17 00:36:03 -0500

    implementacion de objetos rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "inicio",
+    component: InicioComponent
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/inicio"
+  },
+  //lazy loading: precargar los modulos
+  {
+    path: "seguridad",
+    loadChildren: () => import("./modulos/seguridad/seguridad.module")
+                              .then( m => m.SeguridadModule )
+  },
+  {
+    path: "administracion",
+    loadChildren: () => import("./modulos/administracion/administracion.module")
+                              .then( m => m.AdministracionModule )
+  },
+  {
+    path: "pedidos",
+    loadChildren: () => import("./modulos/pedidos/pedidos.module")
+                              .then( m => m.PedidosModule )
+  },
+  {
+    path: "**",
+    component: ErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
